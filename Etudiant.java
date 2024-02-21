0f662d54fde1b86cb6656462c8c135ffe3a25b24
Tanjaoui Nasreddine <nasretanjaoui@gmail.com>
2024-02-21 11:52:02 +0100

    etudiant modifier sur la branche develop

--- Etudiant.java
+++ Etudiant.java
@@ -3,4 +3,14 @@ public class Etudiant{
 	private String nom;
 	private String prenom;
 	private double moyenne;
+
+	public String getNom(){
+		return nom;
+	}
+
+	public String setNom(String nom_etd){
+		this.nom = nom_etd;
+		return nom;
+	}
+	
 }
